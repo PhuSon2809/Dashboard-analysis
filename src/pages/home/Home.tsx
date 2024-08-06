116ebdaf5fde9c634b09ba1a5814c5aa4f368ce1
Hai Dinh <hai.dinh@atech88.com>
2024-08-06 12:04:23 +0700

    add banner 3d

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,6 +2,7 @@ import { memo } from 'react'
 import { Link } from 'react-router-dom'
 import unpleash from '~/assets/images/unpleash.svg'
 import { AutoplayVideo } from '~/components/autoplayVideo'
+import Banner3D from '~/components/banner/banner3D'
 import { ButtonPrimary } from '~/components/button'
 
 interface IHomePageProps {}
@@ -10,17 +11,20 @@ const Home: React.FunctionComponent<IHomePageProps> = memo(() => {
   return (
     <div className='h-auto'>
       <div className='carousel relative'>
-        <div className='flex min-h-screen w-full justify-center lg:pt-[200px]'>
+        <div className='flex min-h-screen w-full justify-center lg:pt-[100px]'>
           <div className='flex flex-col items-center gap-14'>
             <h1 className='max-w-[800px] text-center text-[52px] font-semibold capitalize'>
               With AI, understand customers in-store with ease
             </h1>
             <ButtonPrimary className=''>
-              <Link to='https://analysis.m.pro/' className='h-full w-full'>
+              <Link to='http://pre.fi.ai' className='h-full w-full'>
                 <span>Start</span>
               </Link>
             </ButtonPrimary>
           </div>
+        </div>
+        <div className='absolute left-0 top-0'>
+          <Banner3D />
         </div>
       </div>
       <div className='accurate-count'>
@@ -33,13 +37,13 @@ const Home: React.FunctionComponent<IHomePageProps> = memo(() => {
         </div>
       </div>
       <div className='unleash-the-power container-wrapper grid grid-cols-3 pt-[150px]'>
-        <div className='flex items-end flex-col mr-[-22%] relative '>
-          <h2 className='text-[56px] pr-[150px]'>Unleash the power</h2>
+        <div className='relative mr-[-22%] flex flex-col items-end'>
+          <h2 className='pr-[150px] text-[56px]'>Unleash the power</h2>
           <h1 className='text-[104px] font-bold'>Of Offline </h1>
           <h2 className='text-[56px]'>Commerce </h2>
         </div>
         <div className='col-span-2'>
-          <img src={unpleash} alt='unpleash' className='w-full h-full'/>
+          <img src={unpleash} alt='unpleash' className='h-full w-full' />
         </div>
       </div>
     </div>
