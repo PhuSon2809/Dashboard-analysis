f61ef9d39e6042a3f32bc41b31380c1c4833011e
Lê Khải Hoàn <129830151+yuu-erp@users.noreply.github.com>
2024-08-06 15:39:47 +0700

    update

--- src/components/ListGridListen.tsx
+++ src/components/ListGridListen.tsx
@@ -45,15 +45,7 @@ const ListGridListen  = () => {
   return (
     <React.Fragment>
       <div className="w-full h-full md:flex block items-center justify-center py-5">
-        <div className="grid md:grid-cols-3 grid-cols-1 md:gap-8 gap-8 items-center justify-center">
-          {/* {[...Array(6)].map((_, index) => (
-            <div key={index} className={classNames("relative h-full min-w-[300px] w-full bg-gray-200 rounded-lg", isEven(index) ? "rotate-12" : "-rotate-12" )}>
-              <div className="absolute inset-0 bg-black opacity-50" />
-              <div className="p-4 text-center text-white">
-                Listening #{index + 1}
-              </div>
-            </div>
-          ))} */}
+        <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 md:gap-8 gap-8 items-center justify-center">
          {data.map((user, index) => (
             <InfoUser key={index} {...user} className={user.className} />
           ))}
