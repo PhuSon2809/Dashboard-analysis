06fa49b192dc255a2897f407b7376c398871ff5c
Lê Khải Hoàn <129830151+yuu-erp@users.noreply.github.com>
2024-08-06 15:03:00 +0700

    UPDATE

--- src/components/InfoUser.tsx
+++ src/components/InfoUser.tsx
@@ -1,24 +1,39 @@
 import classNames from 'classnames';
 import React from 'react';
 import { FaInstagram } from "react-icons/fa6";
+import ArrowListen from '~/assets/icons/ArrowListen';
 
 interface Props {
   className?: string;
+  name: string;
+  des: string;
+  avatar: string;
+  role?: string;
 }
 const InfoUser = (props: Props) => {
-  const { className } = props;
+  const { className, name, des, role } = props;
   return (
     <React.Fragment>
-      <div className={classNames("w-ful md:min-w-[360px] min-w-[279px] md:h-[120px] h-[95px] rounded-full bg-[#FFFFFFA3] p-2 flex items-center gap-3", className)}>
-        <div className='h-full aspect-square bg-slate-200 rounded-full'></div>
-        <div className='flex flex-col flex-1'>
-          <div className='text-[#0D0D0D] font-bold text-[20px]'>John Doe</div>
-          <div className='text-[##0D0D0D] text-[14px]'>Software Engineer</div>
-          <div className='flex items-center gap-2 mt-1'>
-            <FaInstagram className="w-6 h-6 text-[#11B0F2]" />
-            <span className='instagram-text'>Instagram</span>
+      <div className={classNames("flex items-center gap-3", className)}>
+        <div className={classNames("w-ful md:min-w-[360px] min-w-[279px] md:h-[120px] h-[95px] rounded-full bg-[#FFFFFFA3] p-2 flex items-center gap-3")}>
+          <div className='h-full aspect-square bg-slate-200 rounded-full overflow-hidden'>
+            <img src={''} className='w-full h-full object-cover' alt="" />
+          </div>
+          <div className='flex flex-col flex-1'>
+            <div className='text-[#0D0D0D] font-bold text-[20px]'>{name}</div>
+            <div className='text-[##0D0D0D] text-[14px]'>{des}</div>
+            <div className='flex items-center gap-2 mt-1'>
+              <FaInstagram className="w-6 h-6 text-[#11B0F2]" />
+              <span className='instagram-text'>Instagram</span>
+            </div>
           </div>
         </div>
+        {role && (
+          <div className='flex items-center gap-3'>
+            <ArrowListen className="w-12 h-12 -rotate-[90deg]" />
+            <span className="absolute md:left-[-20px] left-0 top-[70px] md:top-[90px] font-semibold">{role}</span>
+          </div>
+        )}
       </div>
     </React.Fragment>
   )
--- src/components/ListGridListen.tsx
+++ src/components/ListGridListen.tsx
@@ -1,21 +1,51 @@
-import classNames from "classnames";
 import React from "react";
-import { isEven } from "~/utils/utils";
 import InfoUser from "./InfoUser";
 
 const data = [
   {
-    name: "test",
+    name: "Dulce Press 1",
     des: "United States",
-    avatar: "",
+    avatar: "https://i.pinimg.com/736x/45/1c/60/451c60a1725dbdd7b2f4c8337c4a024b.jpg",
+    className: "md:rotate-[10deg] rotate-[5deg]",
+  },
+  {
+    name: "Dulce Press 2",
+    des: "United States",
+    avatar: "https://i.pinimg.com/736x/45/1c/60/451c60a1725dbdd7b2f4c8337c4a024b.jpg",
+    className: "-rotate-[10deg] -rotate-[5deg]"
+  },
+  {
+    name: "Dulce Press 3",
+    des: "United States",
+    avatar: "https://i.pinimg.com/736x/45/1c/60/451c60a1725dbdd7b2f4c8337c4a024b.jpg",
+    className: "rotate-[10deg] rotate-[5deg]"
+  },
+  {
+    name: "Dulce Press 4",
+    des: "United States",
+    avatar: "https://i.pinimg.com/736x/45/1c/60/451c60a1725dbdd7b2f4c8337c4a024b.jpg",
+     className: "-rotate-[10deg] -rotate-[5deg] flex-row-reverse",
+     role: "Nail Salon"
+  },
+  {
+    name: "Dulce Press 5",
+    des: "United States",
+    avatar: "https://i.pinimg.com/736x/45/1c/60/451c60a1725dbdd7b2f4c8337c4a024b.jpg",
+    className: "rotate-[10deg] rotate-[5deg]"
+  },
+  {
+    name: "Dulce Press 6",
+    des: "United States",
+    avatar: "https://i.pinimg.com/736x/45/1c/60/451c60a1725dbdd7b2f4c8337c4a024b.jpg",
+     className: "-rotate-[10deg] -rotate-[5deg]"
   }
 ]
 
 const ListGridListen  = () => {
   return (
     <React.Fragment>
-      <div className="w-full h-full md:flex block items-center justify-center">
-        <div className="grid md:grid-cols-3 grid-cols-1 md:gap-8 gap-3 items-center justify-center">
+      <div className="w-full h-full md:flex block items-center justify-center py-5">
+        <div className="grid md:grid-cols-3 grid-cols-1 md:gap-8 gap-8 items-center justify-center">
           {/* {[...Array(6)].map((_, index) => (
             <div key={index} className={classNames("relative h-full min-w-[300px] w-full bg-gray-200 rounded-lg", isEven(index) ? "rotate-12" : "-rotate-12" )}>
               <div className="absolute inset-0 bg-black opacity-50" />
@@ -24,8 +54,8 @@ const ListGridListen  = () => {
               </div>
             </div>
           ))} */}
-         {[...Array(6)].map((_, index) => (
-            <InfoUser key={index} className={isEven(index) ? "md:rotate-12 rotate-3" : "md:-rotate-12 -rotate-3"} />
+         {data.map((user, index) => (
+            <InfoUser key={index} {...user} className={user.className} />
           ))}
         </div>
       </div>
