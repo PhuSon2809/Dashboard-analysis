4ddf0ef2bd5074ca2785741c2170f26eccfdcb23
Lê Nguyễn Phương Thái <thailnp1303@gmail.com>
2024-08-06 18:36:39 +0700

    fix ui

--- src/sections/home/RealTimeReport.tsx
+++ src/sections/home/RealTimeReport.tsx
@@ -3,16 +3,16 @@ import { TimelineChart } from '~/components/chart'
 
 const RealTimeReport = memo(() => {
   return (
-    <div className='xs:px-4 sm:px-0 mt-[120px]'>
-      <div className='xs:space-y-[14px] sm:space-y-4 text-center relative z-20'>
-        <h2 className='xs:text-[32px]/[38px] sm:text-[54px]/[70.2px] font-bold'>Real - Time Report</h2>
-        <p className='xs:leading-[26px] sm:leading-[24px] text-grey999/[.64]'>
+    <div className='mt-[120px] xs:px-4 sm:px-0'>
+      <div className='relative z-20 text-center xs:space-y-[14px] sm:space-y-4'>
+        <h2 className='font-bold xs:text-[32px]/[38px] sm:text-[54px]/[70.2px]'>Real - Time Report</h2>
+        <p className='text-grey999/[.64] xs:leading-[26px] sm:leading-[24px]'>
           We offer up-to-the-minute account of <br className='xs:flex sm:hidden' /> current events or data, provided{' '}
           <br /> immediately as the situation unfolds.
         </p>
       </div>
 
-      <div className='w-[1216px] h-[542px] bg-grey100 rounded-3xl shadow-s-3 mx-auto mt-[60px]'>
+      <div className='mx-auto mt-[60px] h-[542px]  max-w-[1216px] rounded-3xl bg-grey100 shadow-s-3'>
         <TimelineChart />
       </div>
     </div>
